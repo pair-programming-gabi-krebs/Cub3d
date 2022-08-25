39144756b771f8e0dbaf13a718e2a63d34c558d1
gcosta-d <gcosta-d@student.42sp.org.br>
2022-08-24 22:57:52 -0300

    add: close window function

--- src/main/close_window.c
+++ src/main/close_window.c
@@ -6,16 +6,17 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 22:52:20 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/08/24 22:53:47 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/08/24 22:57:27 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cube.h"
 
-void	close_window(t_cube *cube)
+int	close_window(t_cube *cube)
 {
 	mlx_destroy_window(cube->mlx_ptr, cube->mlx_win);
 	mlx_destroy_display(cube->mlx_ptr);
 	free(cube->mlx_ptr);
 	exit(0);
+	return (0);
 }
