2ff8721bd8f028703f825462278e0591a5633eed
KrebsCoder <lucasklopess@gmail.com>
2022-09-08 23:09:57 -0300

    add: new defines and function prototype

--- includes/cube.h
+++ includes/cube.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cube.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 01:54:44 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/01 05:01:21 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/09/08 23:06:57 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define CUBE_H
 
 # include <mlx.h>
+# include <math.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
 # include "../libs/libft.h"
 
 # define WRONG_FILE "Enter a valid map"
+# define M_PI 3.14159265358979323846
 # define WRONG_EXTENSIONS "Enter a valid .cub file"
 # define NO "NO"
 # define SO "SO"
@@ -28,6 +30,7 @@
 # define CLICK_X 17
 # define SIZE_IMG 64
 # define ESC 65307
+# define FOV 60
 # define QUIT 113
 # define KEY_PRESS 2
 # define A 97
@@ -44,6 +47,7 @@ typedef struct s_player
 	void	*testeImg;
 	void	*paredeImg;
 	void	*personagemImg;
+	int		rotation_angle;
 	int		img_width;
 	int		img_height;
 	int		pos_x;
@@ -65,6 +69,7 @@ typedef struct s_cube
 	t_player	player;
 }	t_cube;
 
+
 // Validations
 void	file_validations(t_cube *cube, int arguments, char *file);
 void	content_validations(t_cube *cube, int fd);
@@ -78,5 +83,6 @@ int		key_map(int key, t_cube *cube);
 // Utils
 void	ft_exit(char *message);
 int		strict_strcmp(const char *s1, const char *s2);
+double	deg_to_rad(double degree);
 
 #endif
