caa00be78ab1bc9f2d618a7bec9143ebe1eb1e24
gabrielasertori <gabriela@sertori.com.br>
2022-09-14 05:25:03 +0200

    refactor: game function render only has two main functions, keep the current position of the player and cast de current rays

--- src/main/game.c
+++ src/main/game.c
@@ -3,16 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   game.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 03:11:52 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/08 23:05:27 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/09/14 05:16:15 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cube.h"
 
 static int	render(t_cube *cube);
+static void cast_all_rays(t_cube *cube);
+static void update_player(t_cube *cube);
+static void render_player(t_cube *cube);
+
 
 void	game(t_cube *cube)
 {
@@ -24,9 +28,37 @@ void	game(t_cube *cube)
 
 static int	render(t_cube *cube)
 {
-	put_image_to_window(cube);
-	cast_all_rays(cube);
+	update_player(cube);
+	render_player(cube);
 	return (0);
+}
+
+static void update_player(t_cube *cube)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (cube->map[y][0])
+	{
+		x = 0;
+		while (cube->map[y][x])
+		{
+			if (cube->map[y][x] == 'N' || cube->map[y][x] == 'W' || cube->map[y][x] == 'E' || cube->map[y][x] == 'S')
+			{
+				cube->player.pos_x = x;
+				cube->player.pos_y = y;
+			}
+			x++;
+		}
+		y++;
+	}	
+}
+
+
+static void render_player(t_cube *cube)
+{
+	cast_all_rays(cube);
 }
 
 static void cast_all_rays(t_cube *cube)
@@ -38,29 +70,4 @@ static void cast_all_rays(t_cube *cube)
 	// colocar rotation_angle na struct
 	ray_angle = cube->player.rotation_angle - (deg_to_rad(FOV) / 2);
 	
-}
-
-static void put_image_to_window(t_cube *cube)
-{
-	int	x;
-	int	y;
-
-	y = -1;
-	while (cube->map[++y])
-	{
-		x = -1;
-		while (cube->map[y][++x])
-		{
-			if (cube->map[y][x] == '1')
-				mlx_put_image_to_window(cube->mlx_ptr, cube->mlx_win, cube->player.paredeImg, x * SIZE_IMG, y * SIZE_IMG);
-			else if (cube->map[y][x] == '0')
-				mlx_put_image_to_window(cube->mlx_ptr, cube->mlx_win, cube->player.testeImg, x * SIZE_IMG, y * SIZE_IMG);
-			else if (cube->map[y][x] == 'N')
-			{
-				mlx_put_image_to_window(cube->mlx_ptr, cube->mlx_win, cube->player.personagemImg, x * SIZE_IMG, y * SIZE_IMG);
-				cube->player_x = x;
-				cube->player_y = y;
-			}
-		}
-	}	
 }
