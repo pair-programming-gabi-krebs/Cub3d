b24bfce3df907a896bce442306130957bb5e53d8
gabrielasertori <gabriela@sertori.com.br>
2022-09-16 04:03:32 +0200

    add: key press and release hooks

--- src/main/game.c
+++ src/main/game.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 03:11:52 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/14 05:16:15 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/09/16 03:44:25 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ static void render_player(t_cube *cube);
 
 void	game(t_cube *cube)
 {
-	mlx_hook(cube->mlx_win, KEY_PRESS, 1L << 0, key_map, cube);
+	mlx_hook(cube->mlx_win, KEY_PRESSED, 1L << 0, key_press, cube);
+	mlx_hook(cube->mlx_win, KEY_RELEASED, 1L << 1, key_release, cube);
 	mlx_hook(cube->mlx_win, CLICK_X, 0, close_window, cube);
 	mlx_loop_hook(cube->mlx_ptr, render, cube);
 	mlx_loop(cube->mlx_ptr);	
@@ -35,26 +36,9 @@ static int	render(t_cube *cube)
 
 static void update_player(t_cube *cube)
 {
-	int	x;
-	int	y;
-
-	y = 0;
-	while (cube->map[y][0])
-	{
-		x = 0;
-		while (cube->map[y][x])
-		{
-			if (cube->map[y][x] == 'N' || cube->map[y][x] == 'W' || cube->map[y][x] == 'E' || cube->map[y][x] == 'S')
-			{
-				cube->player.pos_x = x;
-				cube->player.pos_y = y;
-			}
-			x++;
-		}
-		y++;
-	}	
+	printf("Walk dir: %f\n", cube->player.walk_direction);
+	printf("Turn dir: %f\n", cube->player.turn_direction);
 }
-
 
 static void render_player(t_cube *cube)
 {
@@ -69,5 +53,4 @@ static void cast_all_rays(t_cube *cube)
 	column_id = 0;
 	// colocar rotation_angle na struct
 	ray_angle = cube->player.rotation_angle - (deg_to_rad(FOV) / 2);
-	
 }
