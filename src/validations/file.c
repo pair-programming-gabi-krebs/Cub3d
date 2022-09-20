2e760fb80671c0967f0f3084026eb592f289979b
KrebsCoder <lucasklopess@gmail.com>
2022-09-20 04:17:07 +0200

    fix: norm

--- src/validations/file.c
+++ src/validations/file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   file.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 22:30:39 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/08/24 22:32:47 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/09/20 01:54:09 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ static int open_file(char *file);
 void	file_validations(t_cube *cube, int arguments, char *file)
 {
 	if (file && arguments == 2)
-    {
-    	path_validation(file);
+	{
+		path_validation(file);
 		content_validations(cube, open_file(file));
 	}
 	else
@@ -28,19 +28,19 @@ void	file_validations(t_cube *cube, int arguments, char *file)
 
 static void path_validation(char *file)
 {
-        char    *dot;
+	char	*dot;
 
-        dot = ft_strrchr(file, '.');
-        if (strict_strcmp(dot, ".cub"))
-            ft_exit(WRONG_EXTENSIONS);
+	dot = ft_strrchr(file, '.');
+	if (strict_strcmp(dot, ".cub"))
+		ft_exit(WRONG_EXTENSIONS);
 }
 
 static int open_file(char *file)
 {
-    int fd;
+	int	fd;
 
-    fd = open(file, O_RDONLY);
-    if (!fd)
-        ft_exit(WRONG_FILE);
-    return (fd);
+	fd = open(file, O_RDONLY);
+	if (!fd)
+		ft_exit(WRONG_FILE);
+	return (fd);
 }
