ea861295104523db2a22f65ae99b51a704fc05eb
KrebsCoder <lucasklopess@gmail.com>
2022-09-22 03:30:55 +0200

    feat: now moving is working

--- src/main/key_press.c
+++ src/main/key_press.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 04:33:29 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/22 02:47:09 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/09/22 03:23:08 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	key_press(int key, t_cube *cube)
 	if (key == ESC || key == QUIT)
 		close_window(cube);
 	else if (key == A)
-		cube->player.walk_direction = -1;
+		cube->player.walk_direction -= 0.02;
 	else if (key == W)
-		cube->player.walk_direction = +1;
+		cube->player.walk_direction += 0.02;
 	else if (key == S)
-		cube->player.walk_direction = -1;
+		cube->player.walk_direction -= 0.02;
 	else if (key == D)
-		cube->player.walk_direction = +1;
+		cube->player.walk_direction += 0.02;
 	else if (key == ARROW_LEFT)
 		cube->player.rotation_angle -= 0.2;
 	else if (key == ARROW_RIGHT)
