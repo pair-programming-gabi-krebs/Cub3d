949fcec4b148a3a0c89c05a363b6f59957e14ec5
KrebsCoder <lucasklopess@gmail.com>
2022-09-22 02:52:48 +0200

    fix: now the arrow is more reliable

--- src/main/key_press.c
+++ src/main/key_press.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 04:33:29 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/20 04:23:09 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/09/22 02:47:09 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	key_press(int key, t_cube *cube)
 	else if (key == D)
 		cube->player.walk_direction = +1;
 	else if (key == ARROW_LEFT)
-		cube->player.turn_direction = -1;
+		cube->player.rotation_angle -= 0.2;
 	else if (key == ARROW_RIGHT)
-		cube->player.turn_direction = +1;
+		cube->player.rotation_angle += 0.2;
 	if (key == A || key == W || key == S || key == D || key == ARROW_LEFT || key == ARROW_RIGHT)
 		cube->player.has_updated = 1;
 	return (0);
