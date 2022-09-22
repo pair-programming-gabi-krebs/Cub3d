bafd04d0fceffe5ce7e97e42178ecd51ad01270d
KrebsCoder <lucasklopess@gmail.com>
2022-09-22 02:53:38 +0200

    feat: small changes

--- src/utils/ft_init.c
+++ src/utils/ft_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 23:22:28 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/17 08:08:06 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/09/22 02:49:12 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_init(t_cube *cube)
 	cube->player.turn_direction = 0;
 	cube->player.walk_direction = 0;
 	cube->player.move_speed = 3.0;
-	cube->player.rotation_speed = 3 * (M_PI / 180);
+	cube->player.rotation_speed = 0.03 * (M_PI / 180);
 	cube->player.has_updated = 0;
 	// think of a function that returns the screen size
 	cube->north_pattern = NULL;
