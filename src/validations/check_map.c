1b2c08268bad767fcfa7bd1e6160b3e5260d55ba
KrebsCoder <lucasklopess@gmail.com>
2022-09-30 04:55:14 +0200

    add: new validations

--- src/validations/check_map.c
+++ src/validations/check_map.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 00:45:05 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/30 02:54:00 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/09/30 04:53:44 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 static int	check_first_row(t_cube *cube);
 static int	check_last_row(t_cube *cube);
-static int	check_valid_chars(char *s);
+static int	check_valid_chars_line(char *s);
 static int	map_last_line(t_cube *cube);
-static int	check_body(t_cube *cube);
+static int	check_around_body(t_cube *cube);
+static void	parse_line(char **str, char *arg);
+static int	check_around_spaces(t_cube *cube);
+static int	check_space(t_cube *cube, int i, int j);
 
 
 int	check_map(t_cube *cube)
 {
 	if (check_first_row(cube) 
-		&& check_body(cube)
-		&& check_last_row(cube))
+		&& check_last_row(cube)
+		&& check_around_body(cube)
+		&& check_around_spaces(cube))
 		return (1);
 	return (0);
 }
 
 static int	check_first_row(t_cube *cube)
 {
-	if (!check_valid_chars(cube->map.map[0]))
+	if (!check_valid_chars_line(cube->map.map[0]))
 	{
 		printf("parei no first row\n");
 		return (0);
@@ -40,7 +44,7 @@ static int	check_first_row(t_cube *cube)
 
 static int	check_last_row(t_cube *cube)
 {
-	if (!check_valid_chars(cube->map.map[map_last_line(cube)]))
+	if (!check_valid_chars_line(cube->map.map[map_last_line(cube)]))
 	{
 		printf("parei no last row\n");
 		return (0);
@@ -48,7 +52,7 @@ static int	check_last_row(t_cube *cube)
 	return (1);
 }
 
-static int	check_valid_chars(char *s)
+static int	check_valid_chars_line(char *s)
 {
 	int	i;
 	
@@ -72,43 +76,84 @@ static int	map_last_line(t_cube *cube)
 	return (i - 1);
 }
 
-// static int	check_body(t_cube *cube)
-// {
-// 	int		len;
-// 	int		i;
-// 	//int		j;
-// 	char	*string_dup;
-// 	char	*string_trimmed;
+static int	check_around_body(t_cube *cube)
+{
+	int		len;
+	int		i;
 
-// 	i = 1;
-// 	while (cube->map.map[i])
-// 	{
-// 		string_dup = ft_strdup(cube->map.map[i]);
-// 		string_trimmed = ft_strtrim(string_dup, " ");
-// 		free(string_dup);
-// 		string_dup = ft_strdup(string_trimmed);
-// 		free(string_trimmed);
-// 		string_trimmed = ft_strtrim(string_dup, "\t");
-// 		free(string_dup);
-// 		string_dup = ft_strdup(string_trimmed);
-// 		free(string_trimmed);
-// 		string_trimmed = ft_strtrim(string_dup, "\r");
-// 		free(string_dup);
-// 		string_dup = ft_strdup(string_trimmed);
-// 		free(string_trimmed);
-// 		string_trimmed = ft_strtrim(string_dup, "\n");
-// 		free(string_dup);
-// 		string_dup = ft_strdup(string_trimmed);
-// 		free(string_trimmed);
-// 		len = ft_strlen(string_dup);
-// 		if (string_dup[0] != '1' && string_dup[len - 1] != '1')
-// 		{
-// 			printf("parei no body row: %d\n", i);
-// 			return (0);
-// 		}
-// 		i++;
-// 	}
-// 	return (1);
-// }
+	char	*string_dup;
 
+	i = 1;
+	while (cube->map.map[i])
+	{
+		string_dup = ft_strdup(cube->map.map[i]);
+		parse_line(&string_dup, " ");
+		parse_line(&string_dup, "\t");
+		parse_line(&string_dup, "\r");
+		parse_line(&string_dup, "\n");
+		len = ft_strlen(string_dup);
+		printf("%s\n", string_dup);
+		if ((string_dup[0] && string_dup[len - 1]) && (string_dup[0] != '1' || \
+			string_dup[len - 1] != '1'))
+		{
+			printf("parei no body row: %d\n", i);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
+static int check_around_spaces(t_cube *cube)
+{
+	int		i;
+	int		j;
+	
+
+	i = 0;
+	while (cube->map.map[i])
+	{
+		j = 0;
+		while (cube->map.map[i][j])
+		{
+			if (!check_space(cube, i , j))
+			{
+				printf("ok n\n");
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+static int	check_valid_chars(char chr)
+{
+	if (chr != 32 && !(chr >= 9 && chr <= 13) && chr != 49)
+		return (0);
+	return (1);
+}
+
+static int	check_space(t_cube *cube, int i, int j)
+{
+	if (cube->map.map[i][j] && check_valid_chars(cube->map.map[i][j]))
+	{
+		if ((cube->map.map[i - 1] && check_valid_chars(cube->map.map[i - 1][j]))
+			&& (cube->map.map[i + 1] && check_valid_chars(cube->map.map[i + 1][j]))
+			&& (cube->map.map[i] && check_valid_chars(cube->map.map[i][j - 1]))
+			&& (cube->map.map[i] && check_valid_chars(cube->map.map[i][j + 1])))
+			return (1);
+	}
+	return (0);
+}
+
+static void	parse_line(char **str, char *arg)
+{
+	char	*string_trimmed;
+
+	string_trimmed = ft_strtrim(*str, arg);
+	free(*str);
+	*str = ft_strdup(string_trimmed);
+	free(string_trimmed);
+}
