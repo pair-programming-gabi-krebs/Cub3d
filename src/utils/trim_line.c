36f78e2a421914de0dac84a8733d295e989f48c1
KrebsCoder <lucasklopess@gmail.com>
2022-10-05 18:14:45 +0200

    refactor: now parse_line calls trim_line

--- src/utils/trim_line.c
+++ src/utils/trim_line.c
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_line.c                                       :+:      :+:    :+:   */
+/*   trim_line.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 05:12:39 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/10/05 05:17:23 by lkrebs-l         ###   ########.fr       */
+/*   Created: 2022/10/05 02:08:23 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/10/05 05:17:46 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cube.h"
 
-void    parse_line(char **line)
+void	trim_line(char **str, char *arg)
 {
-    trim_line(line, " ");
-	trim_line(line, "\t");
-	trim_line(line, "\r");
-	trim_line(line, "\n");
+	char	*string_trimmed;
+
+	string_trimmed = ft_strtrim(*str, arg);
+	free(*str);
+	*str = ft_strdup(string_trimmed);
+	free(string_trimmed);
 }
