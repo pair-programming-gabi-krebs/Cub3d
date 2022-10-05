b47fdd4a9f057cf95adf5b8a3dc549fd90c4bb26
KrebsCoder <lucasklopess@gmail.com>
2022-10-05 18:16:39 +0200

    add: new function to reduce lines on check_map

---
+++ src/validations/map/check_border_line.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_border_line.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 05:27:23 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/10/05 05:29:34 by lkrebs-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/cube.h"
+
+int check_border_line(char *line)
+{
+    int len;
+
+    len = ft_strlen(line) - 1;
+    if ((!line[0] || !line[len - 1]) || (line[0] != '1' || \
+			line[len - 1] != '1'))
+	{
+        free(line);
+        return (1);
+    }
+    return (0);
+}
