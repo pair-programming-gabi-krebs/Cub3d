9cf16294184a90f6fe081ef1b13b219d9428e94c
KrebsCoder <lucasklopess@gmail.com>
2022-10-06 04:56:53 +0200

    refactor: check_body is almost working as it should

--- src/validations/map/check_body.c
+++ src/validations/map/check_body.c
@@ -6,34 +6,88 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 02:06:49 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/10/05 18:27:07 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/10/06 04:52:12 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cube.h"
 
-/* static int end_of_file(char **map); */
+
+static int	check_line(t_cube *cube, int i, char *line);
+static void	equalize_map(char **map);
 
 int	check_body(t_cube *cube)
 {
-	int		len;
 	int		i;
 	char	*line;
 
-	i = 1;
+	equalize_map(cube->map.map);
+	i = 0;
 	while (cube->map.map[i])
 	{
 		line = ft_strdup(cube->map.map[i]);
-		len = ft_strlen(line) - 1;
 		parse_line(&line);
-		if ((!line[0] || !line[len - 1]) || (line[0] != '1' || \
-			line[len - 1] != '1'))
-		{
-			free(line);
+		if (check_border_line(line) || !check_line(cube, i, cube->map.map[i]))
 			return (0);
-		}
 		free(line);
 		i++;
 	}
 	return (1);
+}
+
+static int check_line(t_cube *cube, int i, char *line)
+{
+	int	j;
+
+	j = 1;
+	while (line[j])
+	{
+		if (line[j] == ' ')
+		{
+			if (cube->map.map[i] && (!check_valid_char(cube->map.map[i][j - 1])
+				|| (cube->map.map[i] && !check_valid_char(cube->map.map[i][j + 1]))
+				|| (cube->map.map[i + 1] && !check_valid_char(cube->map.map[i + 1][j]))
+				|| (i > 0 && (cube->map.map[i - 1] && !check_valid_char(cube->map.map[i + 1][j])))))
+				return (0);
+		}
+		j++;
+	}
+	return (1);
+}
+
+static void	equalize_map(char **map)
+{
+	int		i;
+	size_t	line_len;
+	size_t	biggest_line;
+	char	*spaces;
+	char	*aux;
+	size_t	diff;
+
+	i = 0;
+	biggest_line = ft_strlen(map[i]);
+	while (map[++i])
+	{
+		if (ft_strlen(map[i]) > biggest_line)
+			biggest_line = ft_strlen(map[i]);	
+	}
+	i = 0;
+	while (map[i])
+	{
+		line_len = ft_strlen(map[i]);
+		if (line_len < biggest_line)
+		{
+			diff = biggest_line - line_len;
+			spaces = malloc(sizeof(char) * (diff));
+			ft_memset(spaces, ' ', diff);
+			map[i][line_len - 1] = ' ';
+			aux = ft_strdup(map[i]);
+			free(map[i]);
+			map[i] = ft_strjoin(aux, spaces);
+			free(aux);
+			free(spaces);
+		}
+		printf("map[i]: %s'\n", map[i]);
+		i++;
+	}
 }
