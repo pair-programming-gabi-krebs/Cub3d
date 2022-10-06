65f7774dc7c34768f33b0240ad73da8b41cba16e
KrebsCoder <lucasklopess@gmail.com>
2022-10-06 04:56:00 +0200

    fix: small change, now it's working properly

--- src/validations/map/check_border_line.c
+++ src/validations/map/check_border_line.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 05:27:23 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/10/05 18:26:31 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/10/06 00:48:20 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	check_border_line(char *line)
 
 	len = ft_strlen(line) - 1;
 	if ((!line[0] || !line[len - 1]) || (line[0] != '1' || \
-		line[len - 1] != '1'))
+		line[len] != '1'))
 	{
 		free(line);
 		return (1);
