556a78f294204798682b8f065c702a4528e0ab48
KrebsCoder <lucasklopess@gmail.com>
2022-10-08 01:55:46 +0200

    refactor: separated equalize_map in two more functions and fixed segfault

--- src/validations/map/check_body.c
+++ src/validations/map/check_body.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 02:06:49 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/10/06 04:52:12 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/10/08 01:52:54 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 static int	check_line(t_cube *cube, int i, char *line);
 static void	equalize_map(char **map);
+static size_t	biggest_str(char **map);
+static void strjoin_spaces(char **map, size_t big_str, size_t len, int i);
 
 int	check_body(t_cube *cube)
 {
@@ -44,10 +46,10 @@ static int check_line(t_cube *cube, int i, char *line)
 	{
 		if (line[j] == ' ')
 		{
-			if (cube->map.map[i] && (!check_valid_char(cube->map.map[i][j - 1])
-				|| (cube->map.map[i] && !check_valid_char(cube->map.map[i][j + 1]))
+			if (cube->map.map[i][j - 1] && (!check_valid_char(cube->map.map[i][j - 1])
+				|| (cube->map.map[i][j + 1] && !check_valid_char(cube->map.map[i][j + 1]))
 				|| (cube->map.map[i + 1] && !check_valid_char(cube->map.map[i + 1][j]))
-				|| (i > 0 && (cube->map.map[i - 1] && !check_valid_char(cube->map.map[i + 1][j])))))
+				|| (i > 0 && (cube->map.map[i - 1] && !check_valid_char(cube->map.map[i - 1][j])))))
 				return (0);
 		}
 		j++;
@@ -55,39 +57,52 @@ static int check_line(t_cube *cube, int i, char *line)
 	return (1);
 }
 
+static size_t	biggest_str(char **map)
+{
+	size_t	str_len;
+	int	i;
+
+	i = 0;
+	str_len = ft_strlen(map[i]);
+	while (map[++i])
+	{
+		if (ft_strlen(map[i]) > str_len)
+			str_len = ft_strlen(map[i]);
+	}
+	return (str_len);
+}
+
+static void strjoin_spaces(char **map, size_t big_str, size_t len, int i)
+{
+	char	*spaces;
+	char	*aux;
+	int		diff;
+
+	diff = big_str - len;
+	spaces = malloc(sizeof(char) * (diff));
+	ft_memset(spaces, ' ', diff);
+	spaces[diff - 1] = '\0';
+	map[i][len - 1] = ' ';
+	aux = ft_strdup(map[i]);
+	free(map[i]);
+	map[i] = ft_strjoin(aux, spaces);
+	free(aux);
+	free(spaces);
+}
+
 static void	equalize_map(char **map)
 {
 	int		i;
 	size_t	line_len;
 	size_t	biggest_line;
-	char	*spaces;
-	char	*aux;
-	size_t	diff;
 
-	i = 0;
-	biggest_line = ft_strlen(map[i]);
-	while (map[++i])
-	{
-		if (ft_strlen(map[i]) > biggest_line)
-			biggest_line = ft_strlen(map[i]);	
-	}
+	biggest_line = biggest_str(map);
 	i = 0;
 	while (map[i])
 	{
 		line_len = ft_strlen(map[i]);
 		if (line_len < biggest_line)
-		{
-			diff = biggest_line - line_len;
-			spaces = malloc(sizeof(char) * (diff));
-			ft_memset(spaces, ' ', diff);
-			map[i][line_len - 1] = ' ';
-			aux = ft_strdup(map[i]);
-			free(map[i]);
-			map[i] = ft_strjoin(aux, spaces);
-			free(aux);
-			free(spaces);
-		}
-		printf("map[i]: %s'\n", map[i]);
+			strjoin_spaces(map, biggest_line, line_len, i);
 		i++;
 	}
 }
