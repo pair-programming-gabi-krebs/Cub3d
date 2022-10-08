46f98fd13cd0228f53e981ecd6bf566080d220e0
KrebsCoder <lucasklopess@gmail.com>
2022-10-08 05:02:14 +0200

    remove unnecessary line

--- src/main/set_map.c
+++ src/main/set_map.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 00:15:37 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/10/08 02:24:09 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/10/08 04:01:14 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,4 @@
 void	set_map(t_cube *cube)
 {
 	printf("%d", cube->content.total_lines);
-	ft_exit(cube, "test");
 }
