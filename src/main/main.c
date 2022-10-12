0a22a91d43598a9e9f4677196f52b2edb461d4af
KrebsCoder <lucasklopess@gmail.com>
2022-10-12 04:49:11 +0200

    refactor: removed set_map and init_window is back

--- src/main/main.c
+++ src/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 01:46:48 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/09/27 00:15:30 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/10/12 01:45:28 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 int	main(int argc, char **argv)
 {
 	t_cube	cube;
-	
+
 	ft_init(&cube);
-    file_validations(&cube, argc, argv[1]);
-	set_map(&cube);
-	//init_window(&cube);
-	//game(&cube);
-    return (0);
+	file_validations(&cube, argc, argv[1]);
+	init_window(&cube);
+	game(&cube);
+	return (0);
 }
