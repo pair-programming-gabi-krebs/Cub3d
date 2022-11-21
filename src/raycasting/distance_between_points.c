de50b4d7bf6b391294402eef4fab8abd65481d24
KrebsCoder <lucasklopess@gmail.com>
2022-11-22 00:59:28 +0100

    feat: new function to calculate distance between two points

---
+++ src/raycasting/distance_between_points.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   distance_between_points.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 23:20:32 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/11/22 00:45:30 by lkrebs-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cube.h"
+
+double	distance_between_points(t_cube *cube, double hit_x, double hit_y)
+{
+	double	pos_x;
+	double	pos_y;
+	double	calc_pos;
+	double	calc_hits;
+
+	pos_x = cube->player.pos_x;
+	pos_y = cube->player.pos_y;
+	calc_pos = ((hit_x - pos_x) * (hit_x - pos_x));
+	calc_hits = ((hit_y - pos_y) * (hit_y - pos_y));
+	return sqtr(calc_pos + calc_hits);
+}
